4615222d983b03914921177e8fa6abc8a6ac3ddf
Tai @ Flex <dch.tai@gmail.com>
2017-01-24 11:43:55 +0000

    refine recipes

--- strongtin.lua
+++ strongtin.lua
@@ -3,14 +3,17 @@
  
 minetest.register_craftitem("tincraft:strong_tin", { 
         description = "Strengthened tin", 
-        inventory_image = "moreores_tin_ingot.png^[colorize:yellow:30" 
+        inventory_image = "moreores_tin_ingot.png^[colorize:violet:30" 
 }) 
  
-core.register_craft({ 
+minetest.register_craftitem("tincraft:metalstonemix", { 
+        description = "Metal and Stone Mix", 
+        inventory_image = "default_coal_lump.png^[colorize:violet:50" 
+}) 
+ 
+minetest.register_craft({ 
         output = "tincraft:strong_tin", 
         recipe = { 
-                {"moreores:tin_ingot","moreores:tin_ingot","moreores:tin_ingot"}, 
-                {"moreores:tin_ingot","default:coal_lump","moreores:tin_ingot"}, 
                 {"moreores:tin_ingot","moreores:tin_ingot","moreores:tin_ingot"}, 
         } 
 }) 
@@ -19,12 +22,18 @@ core.register_craft({
 -- For those who have not been able to mine iron/are afraid of the first mining trip.
 -- Making steel from tin is deliberately laborious.
 
-core.register_craft({
-        output = "default:steel_ingot",
+minetest.register_craft({
+        output = "tincraft:metalstonemix",
         recipe = {
-                {"tincraft:strong_tin","tincraft:strong_tin","tincraft:strong_tin",},
-		{"default:stone","default:stone","default:stone",},
-		{"tincraft:strong_tin","tincraft:strong_tin","tincraft:strong_tin",}
+                {"tincraft:strong_tin","default:stone","tincraft:strong_tin"},
+                {"default:stone","tincraft:strong_tin","default:stone"},
+                {"tincraft:strong_tin","default:stone","tincraft:strong_tin"},
         }
 })
 
+minetest.register_craft({
+	type = "cooking",
+	output = "default:iron_lump",
+	recipe = "tincraft:metalstonemix",
+	cooktime = 10,
+})
